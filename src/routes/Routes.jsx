33a4286e6c77018b5f05284fa1ee28473deee1f8
sajjad244 <mrsajjed@gmail.com>
2024-11-03 20:27:33 +0600

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>i am working</div>,
+  },
+]);
+
+export default routes;
